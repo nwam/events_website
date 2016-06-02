0bbb7c9600828e2420e0bf8642bb8af946c1249d
Noah Murad <noahmurad@hotmail.com>
2016-06-01 23:13:53 -0400

    services page is lookn' good ;)

--- services.php
+++ services.php
@@ -5,6 +5,7 @@
 <head>
   <?php include("head.php"); ?>
   <title>WEBSITE NAME | Services</title>
+  <link rel="stylesheet" href="css/services.css">
 </head>
 
 <body>   
@@ -14,13 +15,22 @@
   <div id="flex-container">
   <div id="left-space"></div>
   <div id="content-wrapper" class="flex-item">
-    <?php include("title_card.php"); ?>
-    <div class="card"><h2>Lorem ipsum</h2> dolor sit amet, consectetur adipiscing elit. Nam eleifend tortor magna. In mollis risus vel neque bibendum eleifend. Donec tincidunt nunc eget diam dignissim eleifend. Proin ut justo nec neque mattis faucibus. Phasellus arcu metus, rutrum in lorem et, sodales euismod lectus. Donec pellentesque vitae quam nec condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec non elit at lectus ornare ornare sit amet eget lacus. Nullam risus risus, cursus ut velit nec, faucibus dictum odio.</div><div class="divider"></div>
-    <div class="card"><h2>Suspendisse aliquam</h2> tempus ex eget ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam vel turpis nisl. Duis euismod consectetur libero, vitae placerat elit congue eu. Nulla dignissim nulla eu odio sodales tempus. Duis sit amet magna ultrices, consequat justo vel, elementum quam. Vestibulum imperdiet sed nisl ac egestas. Proin lorem arcu, ullamcorper sit amet tellus vitae, dictum fermentum augue.</div><div class="divider"></div>
-    <div class="card"><h2>Vivamus sagittis</h2> sapien at felis feugiat fermentum. Aenean sit amet scelerisque nisi. Donec ornare gravida neque, nec ultricies orci convallis quis. Praesent ut interdum velit. Suspendisse viverra purus at elementum lobortis. Nam pellentesque ultrices lacus ac mollis. Duis pulvinar semper venenatis. Curabitur viverra orci quis sapien porttitor, et vulputate dolor ultrices. Morbi vel semper enim. Nunc porttitor dolor et neque vulputate ullamcorper.</div><div class="divider"></div>
-    <div class="card"><h2>Aliquam dictum</h2> tempus urna ac sagittis. Fusce vitae hendrerit mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed id volutpat ante, ut sagittis ligula. Morbi lectus ligula, molestie eget lectus tempor, luctus auctor lorem. Donec consequat aliquam commodo. Nunc nisl est, tristique non pulvinar quis, volutpat scelerisque nunc. In vulputate velit turpis, eu congue eros malesuada eget. Nam rutrum tellus erat, in fermentum velit lobortis ac. Integer fermentum erat rutrum rhoncus dignissim. Maecenas at vestibulum nisi.</div><div class="divider"></div>
-    <div class="card"><h2>Nam quis lobortis</h2> nisi. In hac habitasse platea dictumst. Aliquam ultrices nisl velit, in viverra dolor tincidunt ullamcorper. Integer eget consectetur arcu, vitae pulvinar odio. Sed semper libero urna. Maecenas ultrices, lectus sit amet bibendum fermentum, massa nisi pulvinar tortor, eget vulputate lorem purus non sapien. Mauris tincidunt consectetur nunc, ac dictum tortor. Fusce facilisis mi ut auctor placerat. Proin id sagittis felis. Donec vel rhoncus eros, eu placerat orci. In eu turpis orci. Donec laoreet turpis ante, ut suscipit nisl maximus vel. Proin fermentum scelerisque ullamcorper. Fusce maximus nisl a ex porttitor, quis feugiat justo placerat.</div><div class="divider-last"></div>
-    </div>  
+  <?php include("title_card.php"); ?>
+
+  <div id="fruitjuice" class="card">
+    <span class="card-title"><img class="card-icon" src="img/fruitjuice-icon.png">Fresh Fruit Juice</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend tortor magna. In mollis risus vel neque bibendum eleifend. Donec tincidunt nunc eget diam dignissim eleifend. Proin ut justo nec neque mattis faucibus. Phasellus arcu metus, rutrum in lorem et, sodales euismod lectus. Donec pellentesque vitae quam nec condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec non elit at lectus ornare ornare sit amet eget lacus. Nullam risus risus, cursus ut velit nec, faucibus dictum odio.</p>
+  </div><div class="divider"></div>
+
+  <div id="coffee" class="card">
+    <span class="card-title"><img class="card-icon" src="img/coffee-icon.ico">Rich Ground Coffee</span>
+    <p>tempus ex eget ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam vel turpis nisl. Duis euismod consectetur libero, vitae placerat elit congue eu. Nulla dignissim nulla eu odio sodales tempus. Duis sit amet magna ultrices, consequat justo vel, elementum quam. Vestibulum imperdiet sed nisl ac egestas. Proin lorem arcu, ullamcorper sit amet tellus vitae, dictum fermentum augue.</p>
+  </div><div class="divider"></div>
+
+  <div class="card"><h2>Vivamus sagittis</h2> sapien at felis feugiat fermentum. Aenean sit amet scelerisque nisi. Donec ornare gravida neque, nec ultricies orci convallis quis. Praesent ut interdum velit. Suspendisse viverra purus at elementum lobortis. Nam pellentesque ultrices lacus ac mollis. Duis pulvinar semper venenatis. Curabitur viverra orci quis sapien porttitor, et vulputate dolor ultrices. Morbi vel semper enim. Nunc porttitor dolor et neque vulputate ullamcorper.</div><div class="divider"></div>
+  <div class="card"><h2>Aliquam dictum</h2> tempus urna ac sagittis. Fusce vitae hendrerit mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed id volutpat ante, ut sagittis ligula. Morbi lectus ligula, molestie eget lectus tempor, luctus auctor lorem. Donec consequat aliquam commodo. Nunc nisl est, tristique non pulvinar quis, volutpat scelerisque nunc. In vulputate velit turpis, eu congue eros malesuada eget. Nam rutrum tellus erat, in fermentum velit lobortis ac. Integer fermentum erat rutrum rhoncus dignissim. Maecenas at vestibulum nisi.</div><div class="divider"></div>
+  <div class="card"><h2>Nam quis lobortis</h2> nisi. In hac habitasse platea dictumst. Aliquam ultrices nisl velit, in viverra dolor tincidunt ullamcorper. Integer eget consectetur arcu, vitae pulvinar odio. Sed semper libero urna. Maecenas ultrices, lectus sit amet bibendum fermentum, massa nisi pulvinar tortor, eget vulputate lorem purus non sapien. Mauris tincidunt consectetur nunc, ac dictum tortor. Fusce facilisis mi ut auctor placerat. Proin id sagittis felis. Donec vel rhoncus eros, eu placerat orci. In eu turpis orci. Donec laoreet turpis ante, ut suscipit nisl maximus vel. Proin fermentum scelerisque ullamcorper. Fusce maximus nisl a ex porttitor, quis feugiat justo placerat.</div><div class="divider-last"></div>
+  </div>  
   <div id="right-space"></div>
   </div>
   </div>
